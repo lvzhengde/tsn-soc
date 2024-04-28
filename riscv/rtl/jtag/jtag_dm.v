6c8fef0084857560c37ae286e22f88dfbbc5846d
lvzhengde <luizd@sina.com>
2024-04-28 15:32:32 +0800

    coding jtag_dm.v--4

--- riscv/rtl/jtag/jtag_dm.v
+++ riscv/rtl/jtag/jtag_dm.v
@@ -41,7 +41,9 @@ module jtag_dm
 // Params
 //-----------------------------------------------------------------
 #(
-    parameter DMI_ADDR_W = 7
+    parameter DMI_ADDR_W         = 7,
+    parameter MEM_CACHE_ADDR_MIN = 32'h80000000 ,
+    parameter MEM_CACHE_ADDR_MAX = 32'h8fffffff 
 )
 //-----------------------------------------------------------------
 // Ports
@@ -171,9 +173,11 @@ module jtag_dm
             dtm_req_data_q <= dtm_req_data_i;
     end    
 
-    assign op_w   = dtm_req_data_q[1:0];
-    assign data_w = dtm_req_data_q[33:2];
-    assign addr_w = dtm_req_data_q[DMI_ADDR_W+33:34];
+    wire  [DMI_ADDR_W+33:0] dtm_req_data_w = dm_ack_pul_w ? dtm_req_data_i : dtm_req_data_q;
+    
+    assign op_w   = dtm_req_data_w[1:0];
+    assign data_w = dtm_req_data_w[33:2];
+    assign addr_w = dtm_req_data_w[DMI_ADDR_W+33:34];
 
     assign dm_ack_o = dm_ack_q;
 
@@ -325,56 +329,83 @@ module jtag_dm
         //command completed to release busy
         //non-memory access command completed immediately
         else if (cmdtype_w != 8'h2)
-            busy_q <= 1'b0
-
+            busy_q <= 1'b0;
+        else if (is_mem_access_q && mem_wb_valid) 
+            busy_q <= 1'b0;
     end
+
+    reg   is_mem_access_q;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin 
             cmderr_q        <= 3'h0;
             issue_command_q <= 1'b0;
             command_q       <= 32'h0;
+            is_mem_access_q <= 1'b0;
         end 
         else if(!dmactive_w) begin   //reset DM
             cmderr_q        <= 3'h0; 
             issue_command_q <= 1'b0;
             command_q       <= 32'h0;
+            is_mem_access_q <= 1'b0;
         end
         else if ((cmderr_q == 3'h0) && busy_q && (abstractcs_wrsel_w | command_wrsel_w)) begin 
             cmderr_q        <= 3'h1; 
             issue_command_q <= 1'b0;
+            is_mem_access_q <= 1'b0;
         end
         else if ((cmderr_q == 3'h0) && busy_q && (data0_wrsel_w | data1_wrsel_w | data2_wrsel_w | data0_rdsel_w | data1_rdsel_w | data2_rdsel_w)) begin
             cmderr_q        <= 3'h1; 
             issue_command_q <= 1'b0;
+            is_mem_access_q <= 1'b0;
         end
         else if (abstractcs_wrsel_w) begin //write 1 to clear
             cmderr_q[0]     <= data_w[ 8] ? 0 : cmderr_q[0];
             cmderr_q[1]     <= data_w[ 9] ? 0 : cmderr_q[1];
             cmderr_q[2]     <= data_w[10] ? 0 : cmderr_q[2];
             issue_command_q <= 1'b0;
+            is_mem_access_q <= 1'b0;
         end
         else if ((cmderr_q == 3'h0) && command_wrsel_w) begin
             command_q       <= data_w;
+            issue_command_q <= 1'b0;
+            is_mem_access_q <= 1'b0;
 
             if (cmdtype_w == 8'h0) begin       //Access register
                 if (aarsize_w > 3'h2 || postexec_w == 1'b1 || aarpostincrement_w == 1'b1) begin
-                    cmderr_q        <= 3'h2; 
-                    issue_command_q <= 1'b0;
+                    cmderr_q <= 3'h2; 
                 end
                 else begin
                     issue_command_q <= 1'b1;
                 end
             end
             else if (cmdtype_w == 8'h2) begin  //Access memory
-            
+                if (!hart_halted_q) begin      //hart should be halted when access memory
+                    cmderr_q <= 3'h4;
+                end
+                else if (aamsize_w > 3'h2) begin
+                    cmderr_q <= 3'h2;
+                end
+                else if (mem_unaligned_r) begin
+                    cmderr_q <= 3'h5;
+                end
+                else begin
+                    issue_command_q <= 1'b1;
+                    is_mem_access_q <= 1'b1;
+                end
             end
             else begin  //not supported
                 cmderr_q        <= 3'h2; 
-                issue_command_q <= 1'b0;
             end
         end
-        else begin  //issue_command keep active only 1 cycle
+        else if (is_mem_access_q && mem_wb_valid) begin
+            issue_command_q <= 1'b0;
+            is_mem_access_q <= 1'b0;
+
+            if (mem_d_error_i)
+                cmderr_q <= 3'h5
+        end
+        else begin  //issue command keep active only 1 cycle
             issue_command_q <= 1'b0;
         end
     end
@@ -400,6 +431,8 @@ module jtag_dm
             data0_q <= gpr_data_rd_i;
         else if (is_csr_read_q)
             data0_q <= csr_data_rd_i;
+        else if (is_mem_access_q && !write_w && mem_wb_valid)
+            data0_q <= mem_wb_value;
     end
 
     always @(posedge clk or negedge rst_n) begin
@@ -410,6 +443,14 @@ module jtag_dm
         else if (data1_wrsel_w & (!busy_q)) 
             data1_q <= data_w; 
         //changed by abstract access register/memory
+        else if (is_mem_access_q && aampostincrement_w && mem_wb_valid) begin
+            case (aamsize_w)
+                3'h0: data1_q <= data1_q + 1;
+                3'h1: data1_q <= data1_q + 2;
+                3'h2: data1_q <= data1_q + 4;
+                default: ;
+            endcase
+        end
     end
 
     always @(posedge clk or negedge rst_n) begin
@@ -547,8 +588,337 @@ module jtag_dm
     assign csr_data_wr_o = csr_data_wr_q ;
     assign csr_raddr_o   = csr_raddr_q   ;
 
+    //-----------------------------------------
+    // DM abstract command -- access memory
+    // Reference design: biriscv_lsu.v
+    // ----------------------------------------
+    reg [ 31:0]  mem_addr_q;
+    reg [ 31:0]  mem_data_wr_q;
+    reg          mem_rd_q;
+    reg [  3:0]  mem_wr_q;
+    reg          mem_cacheable_q;
+    
+    reg          mem_load_q;
+    reg          mem_xb_q;
+    reg          mem_xh_q;
+
+    // Outstanding Access Tracking
+    reg pending_lsu_e2_q;
+    
+    wire issue_lsu_e1_w    = (mem_d_rd_o || (|mem_d_wr_o)) && mem_d_accept_i;
+    wire complete_ok_e2_w  = mem_d_ack_i & ~mem_d_error_i;
+    wire complete_err_e2_w = mem_d_ack_i & mem_d_error_i;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            pending_lsu_e2_q <= 1'b0;
+        else if (issue_lsu_e1_w)
+            pending_lsu_e2_q <= 1'b1;
+        else if (complete_ok_e2_w || complete_err_e2_w)
+            pending_lsu_e2_q <= 1'b0;
+    end
+
+    // Delay next instruction if outstanding response is late
+    wire delay_lsu_e2_w = pending_lsu_e2_q && !complete_ok_e2_w;
+
+    // Combinatorial
+    reg [31:0]  mem_addr_r;
+    reg         mem_unaligned_r;
+    reg [31:0]  mem_data_r;
+    reg         mem_rd_r;
+    reg [3:0]   mem_wr_r;
+
+    always @(*) begin
+        mem_addr_r      = 32'h0;
+        mem_data_r      = 32'h0;
+        mem_unaligned_r = 1'b0;
+        mem_wr_r        = 4'b0;
+        mem_rd_r        = 1'b0;
+    
+        mem_addr_r      = data1_q;
+
+        if (aamsize_w == 3'h2)
+            mem_unaligned_r = (mem_addr_r[1:0] != 2'b0);
+        else if (aamsize_w == 3'h1)
+            mem_unaligned_r = mem_addr_r[0];
+    
+        mem_rd_r = (issue_command_q && is_mem_access_q && !write_w && !mem_unaligned_r);
+    
+        if (issue_command_q && is_mem_access_q && (write && aamsize_w == 3'h2) && !mem_unaligned_r)
+        begin
+            mem_data_r  = data0_q;
+            mem_wr_r    = 4'hf;
+        end
+        else if (issue_command_q && is_mem_access_q && (write && aamsize_w == 3'h1) && !mem_unaligned_r)
+        begin
+            case (mem_addr_r[1:0])
+            2'h2 :
+            begin
+                mem_data_r  = {data0_q[15:0], 16'h0};
+                mem_wr_r    = 4'b1100;
+            end
+            default :
+            begin
+                mem_data_r  = {16'h0, data0_q[15:0]};
+                mem_wr_r    = 4'b0011;
+            end
+            endcase
+        end
+        else if (issue_command_q && is_mem_access_q && write && aamsize_w == 3'h0)
+        begin
+            case (mem_addr_r[1:0])
+            2'h3 :
+            begin
+                mem_data_r  = {data0_q[7:0],24'h000000};
+                mem_wr_r    = 4'b1000;
+            end
+            2'h2 :
+            begin
+                mem_data_r  = {{8'h00,data0_q[7:0]},16'h0000};
+                mem_wr_r    = 4'b0100;
+            end
+            2'h1 :
+            begin
+                mem_data_r  = {{16'h0000,data0_q[7:0]},8'h00};
+                mem_wr_r    = 4'b0010;
+            end
+            2'h0 :
+            begin
+                mem_data_r  = {24'h000000,data0_q[7:0]};
+                mem_wr_r    = 4'b0001;
+            end
+            default :
+            ;
+            endcase
+        end
+        else
+            mem_wr_r    = 4'b0;
+    end
+
+    // Sequential
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+        begin
+            mem_addr_q         <= 32'b0;
+            mem_data_wr_q      <= 32'b0;
+            mem_rd_q           <= 1'b0;
+            mem_wr_q           <= 4'b0;
+            mem_cacheable_q    <= 1'b0;
+            mem_load_q         <= 1'b0;
+            mem_xb_q           <= 1'b0;
+            mem_xh_q           <= 1'b0;
+        end
+        // Memory access fault 
+        else if (complete_err_e2_w)
+        begin
+            mem_addr_q         <= 32'b0;
+            mem_data_wr_q      <= 32'b0;
+            mem_rd_q           <= 1'b0;
+            mem_wr_q           <= 4'b0;
+            mem_cacheable_q    <= 1'b0;
+            mem_load_q         <= 1'b0;
+            mem_xb_q           <= 1'b0;
+            mem_xh_q           <= 1'b0;
+        end
+        else if ((mem_rd_q || (|mem_wr_q)) && delay_lsu_e2_w)
+            ;
+        else if (!((mem_d_rd_o || mem_d_wr_o != 4'b0) && !mem_d_accept_i))
+        begin
+            mem_addr_q         <= mem_addr_r;
+            mem_data_wr_q      <= mem_data_r;
+            mem_rd_q           <= mem_rd_r;
+            mem_wr_q           <= mem_wr_r;
+            mem_load_q         <= write_w & is_mem_access_q & issue_command_q;
+            mem_xb_q           <= aamsize_w == 3'h0;
+            mem_xh_q           <= aamsize_w == 3'h1;
+        
+            /* verilator lint_off UNSIGNED */
+            /* verilator lint_off CMPCONST */
+            mem_cacheable_q    <= (mem_addr_r >= MEM_CACHE_ADDR_MIN && mem_addr_r <= MEM_CACHE_ADDR_MAX);
+            /* verilator lint_on CMPCONST */
+            /* verilator lint_on UNSIGNED */
+        end
+    end
+
+    assign mem_d_addr_o       = {mem_addr_q[31:2], 2'b0};
+    assign mem_d_data_wr_o    = mem_data_wr_q;
+    assign mem_d_rd_o         = mem_rd_q & ~delay_lsu_e2_w;
+    assign mem_d_wr_o         = mem_wr_q & ~{4{delay_lsu_e2_w}};
+    assign mem_d_cacheable_o  = mem_cacheable_q;
+    assign mem_d_req_tag_o    = 11'b0;
+    assign mem_d_invalidate_o = 1'b0;
+    assign mem_d_writeback_o  = 1'b0;
+    assign mem_d_flush_o      = 1'b0;
+
+    assign bus_req_o = is_mem_access_q;
+
+    // Stall upstream busy
+    wire stall_w  = ((mem_d_rd_o || mem_d_wr_o != 4'b0) && !mem_d_accept_i) || delay_lsu_e2_w;
+
+    // Response from memory access
+    wire        mem_resp_load_w;
+    wire [31:0] mem_resp_addr_w;
+    wire        mem_resp_byte_w;
+    wire        mem_resp_half_w;
+
+    jtag_dm_fifo
+    #(
+        .WIDTH     (35) ,
+        .DEPTH     (2)  ,
+        .ADDR_W    (1)  
+    )
+    u_mem_request
+    (
+        .clk         (clk)  ,
+        .rst_n       (rst_n),
+    
+        .push_i      (((mem_d_rd_o || (|mem_d_wr_o)) && mem_d_accept_i)) ,
+        .data_in_i   ({mem_addr_q, mem_xh_q, mem_xb_q, mem_load_q}) ,
+        .accept_o    (),
+    
+        .valid_o     (),
+        .data_out_o  ({mem_resp_addr_w, mem_resp_half_w, mem_resp_byte_w, mem_resp_load_w}),
+        .pop_i       (mem_d_ack_i) 
+    );
+
+    //-----------------------------------------------------------------
+    // Load response
+    //-----------------------------------------------------------------
+    reg [1:0]  addr_lsb_r;
+    reg        load_byte_r;
+    reg        load_half_r;
+    reg        load_signed_r;
+    reg [31:0] wb_result_r;
+
+    always @(*) begin
+        wb_result_r   = 32'b0;
+    
+        // Tag associated with load
+        addr_lsb_r    = mem_resp_addr_w[1:0];
+        load_byte_r   = mem_resp_byte_w;
+        load_half_r   = mem_resp_half_w;
+    
+        // Access fault-pass 
+        if (mem_d_ack_i && mem_d_error_i)
+            wb_result_r = mem_resp_addr_w;
+        // Handle responses
+        else if (mem_d_ack_i && mem_resp_load_w)
+        begin
+            if (load_byte_r)
+            begin
+                case (addr_lsb_r[1:0])
+                2'h3: wb_result_r = {24'b0, mem_d_data_rd_i[31:24]};
+                2'h2: wb_result_r = {24'b0, mem_d_data_rd_i[23:16]};
+                2'h1: wb_result_r = {24'b0, mem_d_data_rd_i[15:8]};
+                2'h0: wb_result_r = {24'b0, mem_d_data_rd_i[7:0]};
+                endcase
+            end
+            else if (load_half_r)
+            begin
+                if (addr_lsb_r[1])
+                    wb_result_r = {16'b0, mem_d_data_rd_i[31:16]};
+                else
+                    wb_result_r = {16'b0, mem_d_data_rd_i[15:0]};
+            end
+            else begin
+                wb_result_r = mem_d_data_rd_i;
+            end
+        end
+    end
+
+    wire        mem_wb_valid = mem_d_ack_i;
+    wire [31:0] mem_wb_value = wb_result_r;    
 
 
+endmodule
+
+
+module jtag_dm_fifo
+//-----------------------------------------------------------------
+// Params
+//-----------------------------------------------------------------
+#(
+    parameter WIDTH   = 8,
+    parameter DEPTH   = 4,
+    parameter ADDR_W  = 2
+)
+//-----------------------------------------------------------------
+// Ports
+//-----------------------------------------------------------------
+(
+    // Inputs
+    input               clk       ,
+    input               rst_n     ,
+    input  [WIDTH-1:0]  data_in_i ,
+    input               push_i    ,
+    input               pop_i     ,
+
+    // Outputs
+    output [WIDTH-1:0]  data_out_o ,
+    output              accept_o   ,
+    output              valid_o    
+);
+
+    //-----------------------------------------------------------------
+    // Local Params
+    //-----------------------------------------------------------------
+    localparam COUNT_W = ADDR_W + 1;
+    
+    //-----------------------------------------------------------------
+    // Registers
+    //-----------------------------------------------------------------
+    reg [WIDTH-1:0]   ram_q[DEPTH-1:0];
+    reg [ADDR_W-1:0]  rd_ptr_q;
+    reg [ADDR_W-1:0]  wr_ptr_q;
+    reg [COUNT_W-1:0] count_q;
+
+    integer i;
+
+    //-----------------------------------------------------------------
+    // Sequential
+    //-----------------------------------------------------------------
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+        begin
+            count_q   <= {(COUNT_W) {1'b0}};
+            rd_ptr_q  <= {(ADDR_W) {1'b0}};
+            wr_ptr_q  <= {(ADDR_W) {1'b0}};
+        
+            for (i=0;i<DEPTH;i=i+1)
+            begin
+                ram_q[i] <= {(WIDTH) {1'b0}};
+            end
+        end
+        else begin
+            // Push
+            if (push_i & accept_o)
+            begin
+                ram_q[wr_ptr_q] <= data_in_i;
+                wr_ptr_q        <= wr_ptr_q + 1;
+            end
+        
+            // Pop
+            if (pop_i & valid_o)
+                rd_ptr_q      <= rd_ptr_q + 1;
+        
+            // Count up
+            if ((push_i & accept_o) & ~(pop_i & valid_o))
+                count_q <= count_q + 1;
+            // Count down
+            else if (~(push_i & accept_o) & (pop_i & valid_o))
+                count_q <= count_q - 1;
+        end
+    end
+
+    //-------------------------------------------------------------------
+    // Combinatorial
+    //-------------------------------------------------------------------
+    /* verilator lint_off WIDTH */
+    assign valid_o       = (count_q != 0);
+    assign accept_o      = (count_q != DEPTH);
+    /* verilator lint_on WIDTH */
+    
+    assign data_out_o    = ram_q[rd_ptr_q];
 
 endmodule
 
