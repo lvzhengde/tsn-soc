2fb5f07bd20e03b66a9d479185ba48178e6a039c
lvzhengde <luizd@sina.com>
2024-07-18 15:57:36 +0800

    modify uart_host.v

--- soc/uart/tb/uart_host.v
+++ soc/uart/tb/uart_host.v
@@ -1,104 +1,329 @@
-/*++
-//  Description  : uart test host
-//  File         : uart_host.v  
---*/
+/*+
+ * Copyright (c) 2022-2024 Zhengde
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1 Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * 2 Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * 3 Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+-*/
 
-`timescale 1ns/10fs
+/*+
+ *  Description  : uart test host
+ *  File         : uart_host.v  
+-*/
 
-module uart_host(
-  input      serial_in_i,
-  output     serial_out_o  
+
+module uart_host
+//-----------------------------------------------------------------
+// Params
+//-----------------------------------------------------------------
+#(
+    parameter T_CLK = 10  //100MHz clock
+)
+//-----------------------------------------------------------------
+// Ports
+//-----------------------------------------------------------------
+(
+    input      serial_in_i,
+    output     serial_out_o  
 ); 
-  wire          clk;
+    reg           parity_en ; 
+    reg           msb_first ; 
+    reg           start_polarity;
 
-  reg           parity_en ; 
-  reg           msb_first ; 
-  reg           start_polarity;
+    wire          en_16x_baud;
+    reg           reset_buffer;
 
-  wire          en_16x_baud;
-  reg           reset_buffer;
+    wire [7:0]    rx_data_out ;
+    reg           rx_read_buffer;
+    wire          rx_buffer_data_present;
+    wire          rx_buffer_full;
+    wire          rx_buffer_hfull;     
 
-  wire [7:0]    rx_data_out ;
-  reg           rx_read_buffer;
-  wire          rx_buffer_data_present;
-  wire          rx_buffer_full;
-  wire          rx_buffer_hfull;     
+    reg   [7:0]   tx_data_in         ; 
+    reg           tx_write_buffer    ; 
+    wire          tx_buffer_full     ; 
+    wire          tx_buffer_hfull; 
 
-  reg   [7:0]   tx_data_in         ; 
-  reg           tx_write_buffer    ; 
-  wire          tx_buffer_full     ; 
-  wire          tx_buffer_hfull; 
+    reg   [15:0]  baud_config    ;
 
-  reg   [15:0]  baud_config    ;
 
-  clkgen clkgen (
-    .clk                   (clk ),
-    .rst_n                 (rst_n )
-  );
+    // clock and reset
+    reg clk   = 0;  
+    reg rst_n = 0;
 
-  uart_rx uart_rx(
-    .clk                   (clk ),
-    .rst_n                 (rst_n ),     
-                          
-    .parity_en_i           (parity_en ),     
-    .msb_first_i           (msb_first ),     
-    .start_polarity_i      (start_polarity),
-                          
-    .serial_in_i           (serial_in_i ),
-    .data_out_o            (rx_data_out ),
-    .read_buffer_i         (rx_read_buffer ),
-    .reset_buffer_i        (reset_buffer ),                     
-    .en_16x_baud_i         (en_16x_baud ),
-                        
-    .buffer_data_present_o (rx_buffer_data_present ),
-    .buffer_full_o         (rx_buffer_full ),
-    .buffer_hfull_o        (rx_buffer_hfull ),
-    .buffer_afull_o  ( ),
-    .buffer_aempty_o ( )  
-  );
+    always #(T_CLK/2) clk = ~clk;
 
-  uart_tx uart_tx(
-    .clk                   (clk ) , 
-    .rst_n                 (rst_n ) , 
+    task reset;
+    begin
+        rst_n    = 0;
+        #555 
+        rst_n    = 1;
+    end
+    endtask 
+
+    // UART RX
+    uart_rx uart_rx
+    (
+        .clk                   (clk ),
+        .rst_n                 (rst_n ),     
+                              
+        .parity_en_i           (parity_en ),     
+        .msb_first_i           (msb_first ),     
+        .start_polarity_i      (start_polarity),
+                              
+        .serial_in_i           (serial_in_i ),
+        .data_out_o            (rx_data_out ),
+        .read_buffer_i         (rx_read_buffer ),
+        .reset_buffer_i        (reset_buffer ),                     
+        .en_16x_baud_i         (en_16x_baud ),
                             
-    .parity_en_i           (parity_en ) , 
-    .msb_first_i           (msb_first ) , 
-    .start_polarity_i      (start_polarity),
-                            
-    .data_in_i             (tx_data_in ) , 
-    .write_buffer_i        (tx_write_buffer ) , 
-    .reset_buffer_i        (reset_buffer ) , 
-    .en_16x_baud_i         (en_16x_baud ) , 
-                            
-    .serial_out_o          (serial_out_o ) , 
-    .buffer_full_o         (tx_buffer_full ) , 
-    .buffer_hfull_o    (tx_buffer_hfull ),
-    .buffer_afull_o  ( ),
-    .buffer_aempty_o ( )
-  );
+        .buffer_data_present_o (rx_buffer_data_present ),
+        .buffer_full_o         (rx_buffer_full ),
+        .buffer_hfull_o        (rx_buffer_hfull ),
+        .buffer_afull_o        ( ),
+        .buffer_aempty_o       ( )  
+    );
 
-  baud_generator  baud_generator(
-    .clk                   (clk )   ,
-    .rst_n                 (rst_n)   ,
-                           
-    .baud_config_i         (baud_config )   ,
-    .en_16x_baud_o         (en_16x_baud )
-  );
+    // UART TX
+    uart_tx uart_tx
+    (
+        .clk                   (clk ) , 
+        .rst_n                 (rst_n ) , 
+                                
+        .parity_en_i           (parity_en ) , 
+        .msb_first_i           (msb_first ) , 
+        .start_polarity_i      (start_polarity),
+                                
+        .data_in_i             (tx_data_in ) , 
+        .write_buffer_i        (tx_write_buffer ) , 
+        .reset_buffer_i        (reset_buffer ) , 
+        .en_16x_baud_i         (en_16x_baud ) , 
+                                
+        .serial_out_o          (serial_out_o ) , 
+        .buffer_full_o         (tx_buffer_full ) , 
+        .buffer_hfull_o        (tx_buffer_hfull ),
+        .buffer_afull_o        ( ),
+        .buffer_aempty_o       ( )
+    );
 
-  initial begin
-    parity_en = 0;
-    msb_first = 0;
-    baud_config[15:0] = 16'd68;
-    start_polarity = 0;
+    baud_generator  baud_generator
+    (
+        .clk                   (clk )   ,
+        .rst_n                 (rst_n)   ,
+                               
+        .baud_config_i         (baud_config )   ,
+        .en_16x_baud_o         (en_16x_baud )
+    );
 
-    rx_read_buffer = 1'b0;
-    tx_write_buffer = 1'b0;
-    tx_data_in = 8'h0;
-    reset_buffer = 1'b0;
-  end
+    localparam BASEADDR   = 24'h90_0000 ;
+    localparam CLK_FREQ   = 100000000   ;  //100MHz
+    localparam UART_SPEED = 115200      ;  //Baud rate
+    localparam BAUD_CFG   = CLK_FRQ/(16*UART_SPEED);
 
-  reg  [15:0]   wr_buffer[15:0];
-  reg  [15:0]   rd_buffer[15:0]; 
+    reg    tx_done;
+    reg    rx_terminate;
+
+    initial begin
+        parity_en         = 0;
+        msb_first         = 0;
+        baud_config[15:0] = BAUD_CFG;
+        start_polarity    = 0;
+
+        rx_read_buffer    = 1'b0;
+        tx_write_buffer   = 1'b0;
+        tx_data_in        = 8'h0;
+        reset_buffer      = 1'b0;
+
+        tx_done      = 0;
+        rx_terminate = 0;
+    end
+
+    reg  [ 31:0]  rd_buffer[0:1023]; 
+    reg  [ 31:0]  wr_buffer[0:1023]; 
+
+    //-----------------------------------------------------------------
+    // Tasks
+    //-----------------------------------------------------------------
+    task test_transmit;
+        input integer len;
+        input integer random;
+
+        integer idx, seed;
+    begin
+        tx_write_buffer = 1'b0;
+        tx_data_in      = 8'h0; 
+        seed    = random;
+        tx_done = 0;
+
+        if(len > 1024) begin
+            $display($time,,"%m ERROR length exceed 1024 %x", len);
+            $finish;
+        end
+
+        //prepare transmit data
+        for (idx = 0; idx < len; idx = idx+1) begin
+            if (random == 0)
+                wr_buffer[idx] = idx & 'hff;
+            else
+                wr_buffer[idx] = $random(seed) & 'hff;
+        end
+
+        //transmit data
+        for (idx = 0; idx < len; idx = idx+1) begin
+            wait(~tx_buffer_full);
+            @(posedge clk);
+            tx_write_buffer = 1'b1; 
+            tx_data_in      = wr_buffer[idx][7:0];
+            @(posedge clk);
+            tx_write_buffer = 1'b0;
+            tx_data_in      = 8'h0;
+            @(posedge clk); 
+        end
+        
+        //wait for finishing transmit
+        wait(uart_tx.data_present == 1'b0);
+        repeat(16*11) @(posedge en_16x_baud);
+
+        tx_done = 1;
+    end
+    endtask
+
+    task test_receive;
+        output integer len;
+    begin
+        len = 0;
+
+        while (rx_terminate != 1'b1 && len <= 1024) begin        //get SOF
+            wait(rx_buffer_data_present);
+            @(posedge clk);
+            wr_buffer[len][31:0] = {24'h0, rx_data_out[7:0]};
+            rx_read_buffer = 1'b1;
+            @(posedge clk);
+            rx_read_buffer = 1'b0;
+            @(posedge clk);
+
+            len = len + 1;
+        end
+    end
+    endtask
+
+
+    localparam REQ_WRITE        = 8'had;
+    localparam REQ_READ         = 8'h5a;
+
+    task write_mem;
+        input [31:0]  addr;
+        input [ 7:0]  len ;  // in bytes
+        input integer random;
+
+        integer idx, seed;
+        reg  [31:0] temp;
+        reg  [ 1:0] offset;
+        reg  [ 7:0] data;
+    begin
+        tx_write_buffer = 1'b0;
+        tx_data_in      = 8'h0; 
+        seed    = random;
+        tx_done = 0;
+        offset  = 0;
+        data    = 0;
+
+        //prepare data
+        for (idx = 0; idx < (len/4)+1; idx = idx+1) begin
+            if (random == 0)
+                wr_buffer[idx] = idx;
+            else
+                wr_buffer[idx] = $random(seed);
+        end
+
+        //tx command
+        wait(~tx_buffer_full);
+        @(posedge clk);
+        tx_write_buffer = 1'b1;
+        tx_data_in      = REQ_WRITE;
+        @(posedge clk);
+        tx_write_buffer = 1'b0;
+        tx_data_in      = 8'h0;
+        @(posedge clk);
+
+        //tx length
+        wait(~tx_buffer_full);
+        @(posedge clk);
+        tx_write_buffer = 1'b1;
+        tx_data_in      = len;
+        @(posedge clk);
+        tx_write_buffer = 1'b0;
+        tx_data_in      = 8'h0;
+        @(posedge clk);
+
+        //tx memory address
+        temp = addr;
+        for (idx = 0; idx < 4; idx = idx+1) begin
+            wait(~tx_buffer_full);
+            @(posedge clk);
+            tx_write_buffer = 1'b1;
+            tx_data_in      = temp[31:24];
+            @(posedge clk);
+            tx_write_buffer = 1'b0;
+            tx_data_in      = 8'h0;
+            @(posedge clk);
+
+            temp = {temp[23:0], 8'h0};
+        end
+
+        //tx data
+        idx = 0;
+        while (idx < len) begin
+            offset = idx[1:0]
+            case (offset)
+                2'b00: data = wr_buffer[idx>>2][ 7: 0];
+                2'b01: data = wr_buffer[idx>>2][15: 8];
+                2'b10: data = wr_buffer[idx>>2][23:16];
+                2'b11: data = wr_buffer[idx>>2][31:24];
+            endcase
+
+            wait(~tx_buffer_full);
+            @(posedge clk);
+            tx_write_buffer = 1'b1;
+            tx_data_in      = data;
+            @(posedge clk);
+            tx_write_buffer = 1'b0;
+            tx_data_in      = 8'h0;
+            @(posedge clk);
+
+            idx = idx + 1;
+        end
+
+        //wait for finishing transmit
+        wait(uart_tx.data_present == 1'b0);
+        repeat(16*11) @(posedge en_16x_baud);
+
+        tx_done = 1;
+    end
+    endtask
 
   //++
   //tasks for register write access
